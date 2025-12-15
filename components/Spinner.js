aaa4392d252f9d8f8b6b9ed4459adddcf4e134c5
Krishna Sai <adksai99@gmail.com>
2025-12-15 18:28:19 +0530

    Loading Spinner Added

---
+++ components/Spinner.js
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex justify-center items-center py-12">
+      <div className="h-6 w-6 animate-spin rounded-full border-2 border-primary border-t-transparent" />
+    </div>
+  );
+}
